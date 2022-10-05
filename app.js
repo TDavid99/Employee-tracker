c752c2bcb5834f0214c7806733e85884cbc0cd96
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-10-05 19:54:12 -0400

    add and index

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
     editEmployeeRole,
 } = require("./index");
 const cTable = require("console.table");
+const { response } = require("express");
 
 async function mainMenu() {
     const response = await prompt({
@@ -22,9 +23,38 @@ async function mainMenu() {
             },
           {
                name: "Search all employees",
-               value: "VIEW_EMPL"
+               value: "VIEW_EMPLOYEES",
+          },
+          {
+               name: "Search all roles",
+               value: "VIEW_ROLEs",
+          },
+          {
+             name: "Add a role",
+             value: "ADD_A_ROLE",
+          },
+          {
+              name:"Add a department",
+              value:" ADD_A_DEPARTMENT",
+
+          },
+          {
+              name:"Add an employee",
+              Value:"ADD_A_EMPLOYEE",
+          
+          {
+            name: "upadate an employee role",
+            value: "UPDATE_AN_EMPLOYEE_ROLE",
+          },
+          {
+            name:"STOP",
+            VALUE:"STOP",
           }
+        ],     
+
+            },
 
                                                                                                                                 
-    }}
-}
+        });
+
+    const chocie =response.
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const db = require("./");
  
-async function findAllDepartment() {
+async function SearchAllDepartment() {
     return db.query("SELECT * FROM department;");
 }
-  async function findAllRoles() {
+  async function SearchAllRoles() {
     return db.query(
         "SELECT role.id, role.title, role.salary, department.name, role.department_id FROM role LEFT JOIN department ON employee.role_id = department.id," 
     )
-  }
+  };
+}
+async function SearchEmployees() {
+    return db.query(
+        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manger_id FROM employee LEFT JOIN role ON employee.role_id = role.id;"
+    );
+}
+
+async function createDepartment(name){
+    return db.query("ENTER DEPARMENT (name) VAULES (?);", name);
+
+}
+async
